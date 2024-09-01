7c4e2f8a21920c7c98917f98eda1d6a32a8b091f
Raghu Anand <raghuaanand@gmail.com>
2024-09-01 19:09:47 +0530

    modified backend

--- backend/index.js
+++ backend/index.js
@@ -117,6 +117,6 @@ app.get('/userdata', authenticateToken,  (req, res) => {
   });
   
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
 });
